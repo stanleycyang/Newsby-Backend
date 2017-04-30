d53ca0c0da2c125fb7613d7ec89bb7d554e4a977
stanleycyang <stanley@stanleycyang.com>
2017-04-29 17:26:51 -0700

    Closes #22

--- index.js
+++ index.js
@@ -10,13 +10,5 @@ app.get('/', (request, response) => {
   response.send('Hello from Express!')
 });
 
-app.get('/jake', (req, res) => {
-  res.status(200).json({
-    name: 'Jake',
-    age: 25,
-    height: '6ft'
-  });
-});
-
 // listen on the port
 app.listen(port);
